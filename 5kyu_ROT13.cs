23d07f2f10d213aa409b4a59609cfc669fdad168
Maciej Wójtowicz <57180214+macw97@users.noreply.github.com>
2020-04-01 19:06:53 +0200

    Create 5kyu_ROT13.cs

---
+++ 5kyu_ROT13.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Linq;
+public class Kata
+{
+  public static string Rot13(string message)
+  {
+    char[] rot=message.ToCharArray();
+    for(int i=0;i<rot.Length;i++)
+    {
+      if(rot[i]>='a' && rot[i]<='z')
+      {
+        rot[i]=(char)((rot[i]-'a'+13)%26+'a');
+      }
+      if(rot[i]>='A' && rot[i]<='Z')
+      {
+        rot[i]=(char)((rot[i]-'A'+13)%26+'A');
+      }
+    }
+    return new string(rot);
+  }
+}
