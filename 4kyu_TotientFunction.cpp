20aa6539edadd9826463ae300fbad44cd4fa69d1
Maciej Wójtowicz <57180214+macw97@users.noreply.github.com>
2020-04-27 20:05:55 +0200

    Create 4kyu_TotientFunction.cpp

---
+++ 4kyu_TotientFunction.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+unsigned long long properFractions(unsigned long long n)
+{
+  if(n<2)
+  return 0;
+  unsigned long long totient = n;
+  for (long long i = 2; i * i <= n; i++)
+  {
+    if (n % i == 0)
+    {
+      
+      totient *= (i - 1) / (double long)i;
+      while (n % i == 0)
+        n /= i;
+    }
+  }
+  if (n > 1)
+  {
+    totient *= (n - 1) / (double long)n;
+  }
+  return totient;
+}
