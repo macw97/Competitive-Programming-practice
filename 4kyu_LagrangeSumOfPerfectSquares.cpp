8e74dfe7ed1beb6680f6fb89c443c438be3aa3d5
Maciej Wójtowicz <57180214+macw97@users.noreply.github.com>
2020-05-06 00:07:30 +0200

    Create 4kyu_LagrangeSumOfPerfectSquares.cpp

---
+++ 4kyu_LagrangeSumOfPerfectSquares.cpp
@@ -0,0 +1,36 @@
+//https://www.codewars.com/kata/5a3af5b1ee1aaeabfe000084/cpp
+
+#include <iostream>
+#include <math.h>
+using namespace std;
+bool is_perfect(int number)
+{
+  if (number == 1)
+    return true;
+  int root = round(sqrt(number));
+  return number == root * root;
+}
+//Lagrange's Four-square theorem
+//if n can be wrriten in form of 4^k*(8*m + 7) result is 4 
+int sum_of_squares(int n) {
+    if (is_perfect(n))
+    return 1;
+  //n%4==0
+  while ((n & 3) == 0)
+  {
+    n >>= 2;
+  }
+  //n%8==7
+  if ((n & 7) == 7)
+    return 4;
+
+  int sq = (int)sqrt(n);
+  for (int i = 1; i <= sq; i++)
+  {
+    if (is_perfect(n - i * i))
+    {
+      return 2;
+    }
+  }
+  return 3;
+}
